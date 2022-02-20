fece2ebcd2e1687ed2f7e288ccb6a613ed336030
goldfrosch <goldfrosch@daum.net>
2022-02-20 23:54:57 +0900

    GoldFrosch / feat cash control commands
    1. fix cash admin command null error event
    2. feat cash admin add, take, set command method

--- src/main/java/com/goldfrosch/plugin/commands/Commands.java
+++ src/main/java/com/goldfrosch/plugin/commands/Commands.java
@@ -9,6 +9,8 @@ import org.bukkit.entity.Player;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class Commands extends AbstractCommand{
   public Commands(GoldCash plugin, String Command, DataSource dataSource) {
@@ -16,6 +18,44 @@ public class Commands extends AbstractCommand{
   }
 
   private CashQuery cashQuery = new CashQuery(plugin, this.getDataSource());
+  String prefix = plugin.getConfig().getString("message.prefix").replace("&", "§");
+
+  private void adminCashSet(String type, String money, Player player) {
+    switch (type.toUpperCase(Locale.ROOT)) {
+      case "ADD":
+        cashQuery.addCash(player, Integer.parseInt(money));
+      case "TAKE":
+        cashQuery.takeCash(player, Integer.parseInt(money));
+      case "SET":
+        cashQuery.setCash(player, Integer.parseInt(money));
+      default:
+        player.sendMessage(prefix + "잘못된 명령어 입니다.");
+    }
+  }
+
+  private void adminCashSetting(String[] args, Player player) {
+    if (args.length == 3) {
+      if (args[2].matches("[+-]?\\d*(\\.\\d+)?")) {
+        adminCashSet(args[1], args[2], player);
+        player.sendMessage(prefix + "현재 나의 캐시: " + cashQuery.getCash(player) + "원");
+      } else {
+        player.sendMessage(prefix + "숫자를 입력해주세요");
+      }
+    } else if (args.length == 4) {
+      if (args[3].matches("[+-]?\\d*(\\.\\d+)?")) {
+        if (Objects.requireNonNull(Bukkit.getPlayer(args[2])).isOnline()) {
+          adminCashSet(args[1], args[3], Objects.requireNonNull(Bukkit.getPlayer(args[2])));
+          player.sendMessage(prefix + "성공적으로 캐시를 설정했습니다!");
+        } else {
+          player.sendMessage(prefix + "플레이어가 온라인이 아니거나 존재하지 않습니다.");
+        }
+      } else {
+        player.sendMessage(prefix + "숫자를 입력해주세요");
+      }
+    } else {
+      player.sendMessage(prefix + "잘못된 명령어 입니다.");
+    }
+  }
 
   @Override
   public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String alias, String[] args) {
@@ -24,7 +64,6 @@ public class Commands extends AbstractCommand{
 
   @Override
   public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-    String prefix = plugin.getConfig().getString("message.prefix").replace("&", "§");
     if (sender instanceof Player) {
       Player player = (Player) sender;
       if (label.equalsIgnoreCase("cash")) {
@@ -39,34 +78,15 @@ public class Commands extends AbstractCommand{
         } else if (args[0].equalsIgnoreCase("admin")) {
           if (!player.hasPermission("cash.admin")) {
             player.sendMessage(prefix + "권한이 없습니다!");
-            return false;
           } else {
-            if (args[1].isEmpty()) {
+            if (args.length == 1) {
               player.sendMessage(ChatColor.GRAY + "==================================================");
               player.sendMessage(ChatColor.AQUA + "/cash admin" + ChatColor.WHITE + " : 어드민 관련 명령어를 확인합니다.");
               player.sendMessage(ChatColor.GREEN + "/cash admin add" + ChatColor.WHITE + " : 이 플러그인의 도움말을 출력합니다.");
               player.sendMessage(ChatColor.GRAY + "==================================================");
             }
-            if (args[1].equalsIgnoreCase("add")) {
-              if (args[3].isEmpty()) {
-                if (args[2].matches("[+-]?\\d*(\\.\\d+)?")) {
-                  cashQuery.addCash(player, Integer.parseInt(args[2]));
-                  player.sendMessage(prefix + "현재 나의 캐시: " + cashQuery.getCash(player) + "원");
-                } else {
-                  player.sendMessage(prefix + "숫자를 입력해주세요");
-                }
-              } else {
-                if (args[3].matches("[+-]?\\d*(\\.\\d+)?")) {
-                  if (Bukkit.getPlayer(args[2]).isOnline()) {
-                    cashQuery.addCash(Bukkit.getPlayer(args[2]), Integer.parseInt(args[3]));
-                    player.sendMessage(prefix + "성공적으로 캐시를 추가했습니다!");
-                  } else {
-                    player.sendMessage(prefix + "플레이어가 온라인이 아니거나 존재하지 않습니다.");
-                  }
-                } else {
-                  player.sendMessage(prefix + "숫자를 입력해주세요");
-                }
-              }
+            else {
+              adminCashSetting(args, player);
             }
           }
         }
