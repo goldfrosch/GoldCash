ed1a4824456f412030b1f5e497ba05866f188a7b
goldfrosch <goldfrosch@daum.net>
2022-02-20 15:40:27 +0900

    GoldFrosch / fix command error
    1. add error command message

--- src/main/java/com/goldfrosch/plugin/commands/Commands.java
+++ src/main/java/com/goldfrosch/plugin/commands/Commands.java
@@ -27,29 +27,37 @@ public class Commands extends AbstractCommand{
     String prefix = plugin.getConfig().getString("message.prefix").replace("&", "§");
     if (sender instanceof Player) {
       Player player = (Player) sender;
-      if(label.equalsIgnoreCase("cash")){
-        if(args.length == 0){
+      if (label.equalsIgnoreCase("cash")) {
+        if (args.length == 0) {
           player.sendMessage(prefix + "현재 나의 캐시: " + cashQuery.getCash(player) + "원");
-        }
-        else if(args[0].equalsIgnoreCase("help")){
+        } else if (args[0].equalsIgnoreCase("help")) {
           player.sendMessage(ChatColor.GRAY + "==================================================");
           player.sendMessage(ChatColor.AQUA + "/cash" + ChatColor.WHITE + " : 현재 나의 캐시를 확인합니다");
           player.sendMessage(ChatColor.GREEN + "/cash help" + ChatColor.WHITE + " : 이 플러그인의 도움말을 출력합니다.");
           player.sendMessage(ChatColor.GREEN + "/cash get [playerName]" + ChatColor.WHITE + " : [playerName]의 캐시 보유량을 보여줍니다");
           player.sendMessage(ChatColor.GRAY + "==================================================");
-        } else {
-          if(args[0].equalsIgnoreCase("admin")) {
-            if(args[1].equalsIgnoreCase("add")) {
-              if(args[3].isEmpty()) {
-                if(args[2].matches("[+-]?\\d*(\\.\\d+)?")) {
+        } else if (args[0].equalsIgnoreCase("admin")) {
+          if (!player.hasPermission("cash.admin")) {
+            player.sendMessage(prefix + "권한이 없습니다!");
+            return false;
+          } else {
+            if (args[1].isEmpty()) {
+              player.sendMessage(ChatColor.GRAY + "==================================================");
+              player.sendMessage(ChatColor.AQUA + "/cash admin" + ChatColor.WHITE + " : 어드민 관련 명령어를 확인합니다.");
+              player.sendMessage(ChatColor.GREEN + "/cash admin add" + ChatColor.WHITE + " : 이 플러그인의 도움말을 출력합니다.");
+              player.sendMessage(ChatColor.GRAY + "==================================================");
+            }
+            if (args[1].equalsIgnoreCase("add")) {
+              if (args[3].isEmpty()) {
+                if (args[2].matches("[+-]?\\d*(\\.\\d+)?")) {
                   cashQuery.addCash(player, Integer.parseInt(args[2]));
+                  player.sendMessage(prefix + "현재 나의 캐시: " + cashQuery.getCash(player) + "원");
                 } else {
                   player.sendMessage(prefix + "숫자를 입력해주세요");
                 }
-              }
-              else {
-                if(args[3].matches("[+-]?\\d*(\\.\\d+)?")) {
-                  if(Bukkit.getPlayer(args[2]).isOnline()){
+              } else {
+                if (args[3].matches("[+-]?\\d*(\\.\\d+)?")) {
+                  if (Bukkit.getPlayer(args[2]).isOnline()) {
                     cashQuery.addCash(Bukkit.getPlayer(args[2]), Integer.parseInt(args[3]));
                     player.sendMessage(prefix + "성공적으로 캐시를 추가했습니다!");
                   } else {
@@ -61,6 +69,9 @@ public class Commands extends AbstractCommand{
               }
             }
           }
+        }
+        else {
+          player.sendMessage(prefix + "존재하지 않는 명령어 입니다.");
         }
       }
     }
